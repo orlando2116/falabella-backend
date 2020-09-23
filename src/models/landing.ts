c2cb68c0d77eedf33243b6dc89605931c1094267
ogonzalez <orlandoesteban@hotmail.cl>
2020-09-23 17:18:25 -0300

    se agrega rent a objeto

--- src/models/landing.ts
+++ src/models/landing.ts
@@ -4,11 +4,14 @@ export interface ILanding extends Document {
     email: string;
     rut: string;
     phone: string;
+    rent: string;
   }
 const landingScheema = new Schema({
    email: {type: String},
    rut: {type: String},
-   phone: {type: String}  
+   phone: {type: String},
+   rent: {type: String}  
+
 });
 
 export default model<ILanding>('Landing', landingScheema);
